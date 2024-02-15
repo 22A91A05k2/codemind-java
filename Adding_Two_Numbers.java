2b4990847217db93ac99b5ab8c52117bf2598956
22A91A05k2 <119839881+22A91A05k2@users.noreply.github.com>
2024-02-15 11:06:39 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] args){         Scanner cs=new Scanner(System.in);         int i=cs.nextInt();         int j=cs.nextInt();         int z=(i+j);         System.out.printf("%d",z);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner cs=new Scanner(System.in);
+        int i=cs.nextInt();
+        int j=cs.nextInt();
+        int z=(i+j);
+        System.out.printf("%d",z);
+    }
+}
